4c761ac90180a23eb3b44a82ab7407df1a568f53
yasinozkaynk <77541257+yasinozkaynk@users.noreply.github.com>
2023-11-21 16:16:05 +0300

    .

--- TraversalCore/Models/UserRegisterViewModel.cs
+++ TraversalCore/Models/UserRegisterViewModel.cs
@@ -11,6 +11,7 @@ namespace TraversalCore.Models
         public string Name { get; set; }
         public string Surname { get; set; }
         public string UserName { get; set; }
+        public int  Phonenumber { get; set; }
         public string Mail { get; set; }
         public string Password { get; set; }
         [Compare( "Password",ErrorMessage ="Şifreler Uyuşmuyor")]
